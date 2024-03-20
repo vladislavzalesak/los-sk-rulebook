e039fbdcf28ca4ff649614bc89f0363cdcae6a16
Telra <Telra@DESKTOP-6OS0FCO>
2024-03-20 13:27:08 +0100

    commit to test action

--- script.js
+++ script.js
@@ -3,11 +3,28 @@ var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
 function GenerateContentTable(target,initialElement)
 {
     var targetSection = $(target)[0];
-        var elements = [...initialElement.querySelectorAll(":scope > h1,h2,h3,h4,h5")];
-    elements.forEach(item => {
-                console.log(item.textContent);
+    var articles = [...document.querySelectorAll("#root > article")];
+    articles.forEach((article) => {
+            var title = article.querySelector(':scope > h1');
+            var contentTitle = document.createElement('h3');
+            var list = document.createElement('ol');
+            title.querySelector(':scope > br').replaceWith(' - ')
+            contentTitle.innerText = title.innerText;
+            var elements = [...article.querySelectorAll(":scope li>span.header")];
+            elements.forEach(item => {
+                var number = item.parentElement.getAttribute('data-number');
+                var newElement = document.createElement('li');
+                var anchor = document.createElement('a');
+                anchor.href=`#anchor-${number}`;
+                anchor.innerText=item.innerText;
+                newElement.append(anchor);
+                newElement.setAttribute('data-number', number);
+                list.appendChild(newElement);
 
-    });
+            });
+            targetSection.appendChild(contentTitle);
+            targetSection.appendChild(list);
+        });
 
 }
 
@@ -22,13 +39,21 @@ function GetLabel(alpha, number,prefix)
     }}
 
 function generateNumbering(element, prefix) {
-    var items =  [...element.querySelectorAll(':scope > li')];
+    if(element.tagName.toLowerCase() == 'section'.toLowerCase())
+        var scope = ':scope > article'; 
+    else if(element.tagName.toLowerCase() == 'article'.toLowerCase())
+        var scope = ':scope > ol > li';
+    else if(element.tagName.toLowerCase() == 'ol'.toLowerCase())
+        var scope = ':scope > li';
+    var items =  [...element.querySelectorAll(scope)];
     var alpha = element.classList.contains('letter');
     var number = 0;
     items.forEach(li => {
         number++;
         var value = GetLabel(alpha,number,prefix);
-        li.setAttribute('data-number',  value + (prefix=='' ?  '.' : ''));
+        var valueToSet = value + (prefix=='' ?  '.' : '');
+        li.setAttribute('data-number', valueToSet );
+        li.setAttribute('id',`anchor-${valueToSet}`);
         var sublists = [...li.querySelectorAll(':scope > ol')];
         sublists.forEach(ol => {
             generateNumbering(ol, value);
@@ -39,20 +64,23 @@ function generateNumbering(element, prefix) {
 function generateLinkText()
 {
     var links = [...document.querySelectorAll('a[href^="#"]')];
-    var targets = [...document.querySelectorAll('li[id]')];
 
     links.forEach(element => {
+        console.log(element);
         var attributeValue = element.getAttribute('href')
-        var target = document.querySelector('li[id="'+attributeValue.slice(1)+'"]');
+        var target = document.querySelector('li[rule-tag="'+attributeValue.slice(1)+'"]');
         if(target != null) {
+            console.log(target.id);
+            element.href='#'+target.id;
             element.textContent = target.getAttribute('data-number');
+            console.log(element);
             element.onmouseover =  function(e)  {
-                var hover = document.getElementById(attributeValue.slice(1));
+                var hover = document.getElementById(target.id);
                 hover.classList.add('target');
            };
 
            element.onmouseout =  function(e)  {
-            var hover = document.getElementById(attributeValue.slice(1));
+            var hover = document.getElementById(target.id);
             hover.classList.remove('target');
        };
         }
@@ -63,8 +91,10 @@ function generateLinkText()
 
 
 $(document).ready(function()   {
-    var initialElement = $('ol#root')[0];
+    var initialElement = $('section#root')[0];
     generateNumbering(initialElement,'');
     generateLinkText();
     GenerateContentTable('#content-table',initialElement);
 });
+
+
